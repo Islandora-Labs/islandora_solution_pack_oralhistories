2bdf86ca198b0bfa5c227603f33848b91bad45b6
L Jiang <ljiang@utsc>
2014-09-22 12:06:52 -0400

    Add sync transcript display js function.

--- js/oralhistories.js
+++ js/oralhistories.js
@@ -7,62 +7,53 @@
     Drupal.behaviors.islandoraOralHistories = {
         attach: function (context, settings) {
             var enableCaption = Drupal.settings.islandora_oralhistories.enbableTranscriptDisplay;
-            var tracksUploaded = Drupal.settings.islandora_oralhistories.tracks;
+            var racksUploaded = Drupal.settings.islandora_oralhistories.tracks;
             var enableTranscriptDisp = Drupal.settings.islandora_oralhistories.enbableTranscriptDisplay;
-            var oVideo = $('.islandora-oralhistories-content video', context);
+            var oVideo = $('.islandora-oralhistories-content', context).find('video');
+            var oTrackList = oVideo[0].textTracks;
+            var oCues = oTrackList[0].cues;
+            var transcriptContent = $('#transcript-content');
+            var transcripts = [];
+            console.log(oVideo[0]);
+
+
 
             oVideo.on('loadedmetadata', function(){
 
                 if (enableTranscriptDisp) {
-
                     // video.js throw errors when kind="metadata"
                     // codes to solve goes here...
-                    /*
-                     for (i = 0; i<myTextTracks.length; i++) {
-                     switch (myTextTracks[i].mode) {
-                     case 0:
-                     myTextTracks[i].mode = 1;
-                     break;
-                     case 'disabled':
-                     myTextTracks[i].mode = 'showing';
-                     break;
-                     }
-                     }
-
-                     */
-
-                    //loadTranscripts();
-
+                    for (i = 0; i<oTrackList.length; i++) {
+                        switch (oTrackList[i].mode) {
+                            case 0:
+                                oTrackList[i].mode = 1;
+                                break;
+                            case 'disabled':
+                                oTrackList[i].mode = 'showing';
+                                break;
+                        }
+                    }
                 } //end if (enbableTranscriptDisp)
             });
+
+
+
+            // Sync transcript display with media.
+            transcriptContent.on("click",function(e) {
+                if(e.target.id.indexOf("transcript") == 0) {
+                    var i = Number(e.target.id.replace("transcript",""));
+                    oVideo[0].currentTime = $('#transcript' + i).attr('data-start-time');
+                    oVideo[0].play();
+                }
+            });
+
+
 
 
 
         } // end attach function
 
     };
-
-
-
-
-    // Helper function.
-    function loadTranscripts(){
-        var oTrack = document.getElementById('track0');
-        var oTextTrack = oTrack.track;
-        var oCueList = oTextTrack.cues;
-        var transHtml = '';
-        for (i = 0; i < oCueList.length; i++) {
-            var sText = oCueList[i].text;        // get text from first cue
-            var sTime = oCueList[i].startTime;   // get start time from first cue
-            var eTime = oCueList[i].endTime;
-            transHtml += '<p><span id="transcript-line' + i + '" data-start-time="' + sTime + '" ';
-            transHtml += 'data-end-time="' + eTime + '">' + sText + '</span></p>';
-        }
-
-        $('#transcript-content').append(transHtml);
-    };
-
-
 
 })(jQuery);
 
--- theme/islandora-oralhistories.tpl.php
+++ theme/islandora-oralhistories.tpl.php
@@ -25,17 +25,17 @@
         <?php print $islandora_content['viewer']; ?>
       </div>
     <?php endif; ?>
-    <?php if ($islandora_content['transcript_display']): ?>
-      <fieldset id="transcript-display" class="collapsible collapsed">
-        <legend><span class="fieldset-legend">Transcript</span></legend>
-        <div class="fieldset-wrapper">
-          <div id="transcript-content">
-            <?php print $islandora_content['transcript_content']; ?>
-          </div>
-        </div>
-      </fieldset>
-    <?php endif; ?>
   </div>
+  <?php if ($islandora_content['transcript_display']): ?>
+    <fieldset id="transcript-display" class="collapsible collapsed clearfix">
+      <legend><span class="fieldset-legend">Transcript</span></legend>
+      <div class="fieldset-wrapper">
+        <div id="transcript-content">
+          <?php print $islandora_content['transcript_content']; ?>
+        </div>
+      </div>
+    </fieldset>
+  <?php endif; ?>
   <div class="islandora-oralhistories-metadata">
     <?php print $description; ?>
     <?php if ($parent_collections): ?>
