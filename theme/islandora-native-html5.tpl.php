b061d73a560ba067efb209adac7a0dfacbfa99c8
L Jiang <ljiang@utsc>
2014-09-25 14:55:00 -0400

    Adds some basic css styles for native HTML5 video/audio

--- theme/islandora-native-html5.tpl.php
+++ theme/islandora-native-html5.tpl.php
@@ -1,22 +1,22 @@
 <video id="islandora-oralhistories-media-player" class="media-default-skin" controls
-       preload="auto"  poster="<?php print $tn; ?>" data-setup="{}">
-  <?php foreach ($sources as $source): ?>
-    <source src="<?php print $source['url']; ?>" type='<?php print $source['mime']; ?>'>
-  <?php endforeach; ?>
+     preload="auto"  width="640" height="264" poster="<?php print $tn; ?>" data-setup="{}">
+<?php foreach ($sources as $source): ?>
+  <source src="<?php print $source['url']; ?>" type='<?php print $source['mime']; ?>'>
+<?php endforeach; ?>
 
-  <?php foreach ($tracks as $key => $track): ?>
-    <?php if ($key == 0): ?>
-      <?php if ($enable_caption_display): ?>
-        <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
-           kind="captions" label="<?php print $track['lang_code']; ?>" default>
-      <?php else: ?>
-        <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
-           kind="metadata" label="<?php print $track['lang_code']; ?>" default>
-      <?php endif; ?>
+<?php foreach ($tracks as $key => $track): ?>
+  <?php if ($key == 0): ?>
+    <?php if ($enable_caption_display): ?>
+      <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
+         kind="captions" label="<?php print $track['lang_code']; ?>" default>
     <?php else: ?>
       <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
-        kind="subtitles" label="<?php print $track['lang_code']; ?>">
+         kind="metadata" label="<?php print $track['lang_code']; ?>" default>
     <?php endif; ?>
-  <?php endforeach; ?>
-  <p> HTML5 Video/Audio is not supported by this browser.</p>
+  <?php else: ?>
+    <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
+      kind="subtitles" label="<?php print $track['lang_code']; ?>">
+  <?php endif; ?>
+<?php endforeach; ?>
+<p> HTML5 Video/Audio is not supported by this browser.</p>
 </video>
