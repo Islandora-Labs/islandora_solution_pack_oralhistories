a3b3f4e62de4f4cbe9e76f01375c157ae9b6d80c
Lingling Jiang <jiang.iris@gmail.com>
2015-09-14 14:30:16 -0400

    Remove not-working inline search form

--- theme/islandora-transcript-scroller.tpl.php
+++ theme/islandora-transcript-scroller.tpl.php
@@ -1,4 +1,7 @@
-<?php print render($transcript_controls); ?>
+<?php
+  unset($transcript_controls['content']['transcript_search']);
+  print render($transcript_controls);
+?>
 <div class="transcript-container">
   <div class="transcript-content" id="transcript">
     <?php print render($transcript); ?>
