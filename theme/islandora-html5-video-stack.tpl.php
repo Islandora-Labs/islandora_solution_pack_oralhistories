1deca41dbb5b2d801df5ce90354d13adfc7ec63a
Lingling Jiang <jiang.iris@gmail.com>
2015-09-30 15:41:46 -0400

    Use template files

---
+++ theme/islandora-html5-video-stack.tpl.php
@@ -0,0 +1,20 @@
+<div class="row">
+  <div class="col-sm-12 col-md-12">
+    <div data-transcripts-role="video" data-transcripts-id="<?php print $params['trid']; ?>">
+      <div align="center" class="embed-responsive embed-responsive-16by9">
+        <video class="video-js vjs-default-skin embed-responsive-item" controls preload="auto"  width="100%" height="360" data-setup="{}">
+          <source src="<?php print $params['url']; ?>" type="<?php print $params['mime']; ?>"/>
+          <?php if ($params['tracks'] && $params['enable_transcript_display']): ?>
+            <?php foreach ($params['tracks'] as $key => $track): ?>
+              <track id="track<?php print $key; ?>" src="<?php print $track['source_url']; ?>" srclang="<?php print $track['lang_code']; ?>"
+                     kind="captions" label="<?php print $track['lang_code']; ?>" default>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </video>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-12 col-md-12">
+    <?php if (array_key_exists('transcript_content', $params)) { print $params['transcript_content']; } ?>
+  </div>
+</div>
