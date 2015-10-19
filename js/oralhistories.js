28c9c462b51b8971ee7e182c2c6fc3de55ca569f
Lingling Jiang <jiang.iris@gmail.com>
2015-10-19 16:29:49 -0400

    Integrate with ctools modal form for edit transcript

--- js/oralhistories.js
+++ js/oralhistories.js
@@ -11,36 +11,9 @@
             $("#transcript-tabs", context).once(function() {
                     $(this).tabs();
                 });
-            // transcript edit dialog
-            var dialog, form;
-            dialog = $("#transcript-edit-dialog").css("display", "inline-block").dialog({
-                autoOpen: false,
-                height: window.innerHeight * 0.7,
-                minHeight: 300,
-                width: window.innerWidth * 0.7,
-                minWidth: 350,
-                modal: true,
-                buttons: {
-                    "Save" : function(){
-                        $(this).find("form").submit();
-                        $(this).dialog("close");
-                    },
-                    Cancel: function() { $(this).dialog("close");}
-                }
-            });
-            form = dialog.find( "form" ).on( "submit", function( event ) {
-                event.preventDefault();
-                saveTranscript();
-            });
-            function saveTranscript() {console.log("submit transcript")}
-            $("#transcript-edit", context).click(function(){
-                dialog.dialog("open");
-            });
 
         } // end attach function
-
     };
-
 })(jQuery);
 
 
