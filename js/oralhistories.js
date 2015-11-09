7cf5838ceea186d4fa2caaa1466b74112b261939
Lingling Jiang <jiang.iris@gmail.com>
2015-11-09 14:23:25 -0500

    add js for addNewAnnotation checkbox

--- js/oralhistories.js
+++ js/oralhistories.js
@@ -5,13 +5,21 @@
     Drupal.behaviors.islandoraOralHistories = {
         attach: function (context, settings) {
             var targetObjectId = Drupal.settings.islandoraOralhistories.objectId;
+            var enableAnnotation = Drupal.settings.enableAnnotationTabDisplay;
+            var videoElement = $(".islandora-oralhistories-object").find('video, audio')[0];
 
             // Show transcript display as tabs.
             $("#transcript-tabs", context).once(function() {
                     $(this).tabs();
                 });
 
-
+            // Annotation.
+            var addNewAnnotation = $("#add-new-annotation");
+            addNewAnnotation.change(function(){
+                if (this.checked) {
+                    console.log("The addnewannotation is checked.");
+                }
+            });
 
 
         } // end attach function
