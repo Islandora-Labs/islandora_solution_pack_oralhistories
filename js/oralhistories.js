3e7e0f37f2fde23d537610b17dcc06c669c3a141
Lingling Jiang <jiang.iris@gmail.com>
2015-11-09 23:07:03 -0500

    annotation form ajax

--- js/oralhistories.js
+++ js/oralhistories.js
@@ -14,12 +14,25 @@
                 });
 
             // Annotation.
+            var annotationForm = $("#islandora-oralhistories-annotation-form");
+            var annotationFormStart = $("#web-annotation-start-time");
+            var annotationFormSubmit = $("#edit-web-annotation-submit");
             var addNewAnnotation = $("#web-annotation-add-new");
             addNewAnnotation.change(function(){
                 if (this.checked) {
-                    console.log("The addnewannotation is checked.");
+                    if (!videoElement.paused) {
+                        videoElement.pause();
+                        annotationFormStart.val(videoElement.currentTime.toFixed(2));
+                    }
                 }
             });
+            //annotationForm.submit(function() {
+            //    this.submit();
+            //    if (annotationFormSubmit.hasClass("ajax-processed")) {
+            //        $("#web-annotation-container-wrapper").hide();
+            //    }
+            //});
+
 
 
         } // end attach function
