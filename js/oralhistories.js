003f0d124c15ede4facf15e40c8c4b808d88affd
Marcus Barnes <marcus.e.barnes@gmail.com>
2016-11-23 11:01:39 -0500

    Removed lines related to annotations.  See issue #17.

--- js/oralhistories.js
+++ js/oralhistories.js
@@ -6,35 +6,12 @@
         attach: function (context, settings) {
             //console.dir(Drupal.settings.islandoraOralhistories);
             var targetObjectId = Drupal.settings.islandoraOralhistories.objectId;
-            var enableAnnotation = Drupal.settings.enableAnnotationTabDisplay;
             var videoElement = $(".islandora-oralhistories-object").find('video, audio')[0];
 
             // Show transcript display as tabs.
             $("#transcript-tabs", context).once(function() {
                     $(this).tabs();
                 });
-
-            // Annotation.
-            //var annotationForm = $("#islandora-oralhistories-annotation-form");
-            //var annotationFormStart = $("#web-annotation-start-time");
-            //var annotationFormSubmit = $("#edit-web-annotation-submit");
-            //var addNewAnnotation = $("#annotation-add-new-btn");
-            //
-            //addNewAnnotation.once("load-web-annotation-form", function(){
-            //    var ajaxLoadUrl = '/islandora/object/' + targetObjectId + '/web_annotation/add';
-            //    $(this).click(function(){
-            //        $("#web-annotation-form-ajax-loader").load(ajaxLoadUrl, function(){
-            //            Drupal.attachBehaviors();
-            //            if (!videoElement.paused) {
-            //                videoElement.pause();
-            //                $("#web-annotation-start-time").val(videoElement.currentTime.toFixed(2));
-            //            }
-            //        });
-            //
-            //    });
-            //});
-
-
 
         } // end attach function
     };
