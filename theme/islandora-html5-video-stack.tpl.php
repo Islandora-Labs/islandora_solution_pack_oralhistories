5fd93f854bfd0cdad2af1f9181542c963dc8c7c8
Ryan Brinn <ryanbrinn@gmail.com>
2018-03-08 10:57:41 -0400

    added space

--- theme/islandora-html5-video-stack.tpl.php
+++ theme/islandora-html5-video-stack.tpl.php
@@ -2,8 +2,8 @@
   <div class="col-sm-12 col-md-12">
     <div data-transcripts-role="video" data-transcripts-id="<?php print $params['trid']; ?>">
       <div align="center" id="video_container">
-        <video class="video-js vjs-default-skin embed-responsive-item" controls poster="<?php if(isset($params['tn'])){print$params['tn'];} ?>" preload="auto"  width="100%" height="360" data-setup="{}" id="video-js-oh">
-          <source src="<?php if(isset($params['url'])){print $params['url'];} ?>" type="<?php if(isset($params['mime'])){print$params['mime'];} ?>"/>
+        <video class="video-js vjs-default-skin embed-responsive-item" controls poster="<?php if(isset($params['tn'])){print $params['tn'];} ?>" preload="auto"  width="100%" height="360" data-setup="{}" id="video-js-oh">
+          <source src="<?php if(isset($params['url'])){print $params['url'];} ?>" type="<?php if(isset($params['mime'])){print $params['mime'];} ?>"/>
           <?php if (isset($params['tracks']) && $params['enable_transcript_display']): ?>
             <?php foreach ($params['tracks'] as $key => $track): ?>
               <?php if ($track['MEDIATRACK'] == TRUE): ?>
