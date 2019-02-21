d2b9b4dc661bb7b1f54a9425c5c29fca6a226b09
Natkeeran <natkeeran@gmail.com>
2019-02-21 14:04:14 -0500

    update vtt converter lib

--- includes/lib/VttConverter.php
+++ includes/lib/VttConverter.php
@@ -2,31 +2,45 @@
 
 /**
  * Class VttConverter
- * Adobted from here: https://github.com/mantas-done/subtitles
+ * Adopted from here: https://github.com/mantas-done/subtitles/blob/c11531b7a191b97e25235fabc804368a52326108/src/code/Converters/VttConverter.php
+ *
  */
 
 class VttConverter  {
 
   public function fileContentToInternalFormat($file_content)
   {
-    $internal_format = array(); // array - where file content will be stored
+    $internal_format = []; // array - where file content will be stored
 
     $blocks = explode("\n\n", trim($file_content)); // each block contains: start and end times + text
 
     foreach ($blocks as $block) {
-      if (trim($block) == 'WEBVTT') {
+      if(preg_match('/^WEBVTT.{0,}/', $block, $matches)) {
         continue;
       }
+
       $lines = explode("\n", $block); // separate all block lines
+
+      if (strpos($lines[0], '-->') === false) { // first line not containing '-->', must be cue id
+        unset($lines[0]); // not supporting cue id
+        $lines = array_values($lines);
+      }
+
       $times = explode(' --> ', $lines[0]);
+
+      $lines_array = array_map(static::fixLine(), array_slice($lines, 1)); // get all the remaining lines from block (if multiple lines of text)
+      if (count($lines_array) === 0) {
+        continue;
+      }
+
       $name = static::getName($lines[1]);
 
-      $internal_format[] = array(
+      $internal_format[] = [
         'start' => static::vttTimeToInternal($times[0]),
         'end' => static::vttTimeToInternal($times[1]),
         'name' => $name,
-        'lines' => array_map(static::fixLine(), array_slice($lines, 1)), // get all the remaining lines from block (if multiple lines of text)
-      );
+        'lines' => $lines_array,
+      ];
     }
 
     return $internal_format;
@@ -34,16 +48,16 @@ class VttConverter  {
 
   public function internalFormatToFileContent(array $internal_format)
   {
-    $file_content = "WEBVTT\n\n";
+    $file_content = "WEBVTT\r\n\r\n";
 
     foreach ($internal_format as $k => $block) {
       $start = static::internalTimeToVtt($block['start']);
       $end = static::internalTimeToVtt($block['end']);
-      $lines = implode("\n", $block['lines']);
+      $lines = implode("\r\n", $block['lines']);
 
-      $file_content .= $start . ' --> ' . $end . "\n";
-      $file_content .= $lines . "\n";
-      $file_content .= "\n";
+      $file_content .= $start . ' --> ' . $end . "\r\n";
+      $file_content .= $lines . "\r\n";
+      $file_content .= "\r\n";
     }
 
     $file_content = trim($file_content);
@@ -55,7 +69,10 @@ class VttConverter  {
 
   protected static function vttTimeToInternal($vtt_time)
   {
-    $parts = explode('.', '00:' . $vtt_time);
+    $parts = explode('.', $vtt_time);
+
+    // parts[0] could be mm:ss or hh:mm:ss format -> always use hh:mm:ss
+    $parts[0] = substr_count($parts[0], ':') == 2 ? $parts[0] : '00:'.$parts[0];
 
     $only_seconds = strtotime("1970-01-01 {$parts[0]} UTC");
     $milliseconds = (float)('0.' . $parts[1]);
@@ -69,9 +86,9 @@ class VttConverter  {
   {
     $parts = explode('.', $internal_time); // 1.23
     $whole = $parts[0]; // 1
-    $decimal = isset($parts[1]) ? $parts[1] : 0; // 23
+    $decimal = isset($parts[1]) ? substr($parts[1], 0, 3) : 0; // 23
 
-    $srt_time = gmdate("i:s", floor($whole)) . '.' . str_pad($decimal, 3, '0', STR_PAD_RIGHT);
+    $srt_time = gmdate("H:i:s", floor($whole)) . '.' . str_pad($decimal, 3, '0', STR_PAD_RIGHT);
 
     return $srt_time;
   }
@@ -79,10 +96,9 @@ class VttConverter  {
   protected static function fixLine()
   {
     return function($line) {
-      $regex_vtt_name = '/^\s*<v\s+(?<name>.*)>/';
-      preg_match($regex_vtt_name, $line, $matches);
-      if (array_key_exists('name', $matches)) {
-        $line = preg_replace($regex_vtt_name, '', $line);
+      if (substr($line, 0, 3) == '<v ') {
+        $line = substr($line, 3);
+        $line = str_replace('>', ' ', $line);
       }
 
       return $line;
@@ -101,4 +117,5 @@ class VttConverter  {
 
     return $name;
   }
+
 }
